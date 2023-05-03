5c20e995dfb61c6855b0e2e71ad3496755a95904
Pedro Malavasi Betti <malabetti@outlook.com>
2023-05-02 21:41:34 -0300

    Lista 4 - Completa

--- Semana 05/Codigos/src/main/kotlin/fun_lam_os.kt
+++ Semana 05/Codigos/src/main/kotlin/fun_lam_os.kt
@@ -2,6 +2,25 @@ fun calcular(a: Double, b: Double, operacao: (Double, Double) -> Double): Double
     return operacao(a, b)
 }
 
+val maiornum = {list:MutableList<Int> -> list.max()}
+fun somaLista(list1: MutableList<Int>, maior: (MutableList<Int>) -> Int, res1: Int): Int {
+    var list = list1
+    var res = res1
+    if(list.size == 0)
+        return res
+    res += maior(list)
+    list.remove(maior(list))
+    return(somaLista(list, maior, res))
+}
+
+fun somaPor(n2: Int): (Int) -> Int {
+    return {n1: Int -> n1 + n2}
+}
+
+fun printMensagem(n: Int): (String) -> Unit{
+    return {str: String -> (1..n).forEach{println(str)} }
+}
+
 fun main() {
     //1 - Como posso usar uma função lambda para classificar uma lista em ordem crescente em Kotlin?
     val lista = MutableList(10) {(1..10).random() }
@@ -31,4 +50,31 @@ fun main() {
     println("$n1 - $n2 = ${calcular(n1, n2, subtracao)}")
     println("$n1 * $n2 = ${calcular(n1, n2, multiplicacao)}")
     println("$n1 / $n2 = ${calcular(n1, n2, divisao)}")
+
+    //3 - Como posso usar uma função lambda para filtrar elementos de uma lista em Kotlin?
+    val filtro = {list:MutableList<Int>, n:Int -> list.indexOf(n)}
+    val x = (1..10).random()
+    println(lista)
+    println(x)
+    println("Index de $x na lista: ${filtro(lista, x)}")
+
+    //4 - Como posso usar uma função lambda para encontrar o maior valor em uma lista em Kotlin?
+    println("O maior valor da lista é ${maiornum(lista)}")
+
+    //5 - Como posso usar uma função de ordem superior para somar todos os elementos de uma lista em Kotlin?
+    println("Soma dos números: ${somaLista(lista, maiornum, 0)}")
+
+    //6 - Como posso usar uma função de ordem superior para criar uma função que retorna a soma de dois números em Kotlin?
+    val soma2 = somaPor(2)
+    val soma120 = somaPor(120)
+    val resultado1 = soma2(10)
+    println("10 somado por 2 é $resultado1")
+    val resultado2 = soma120(10)
+    println("10 somado por 120 é $resultado2")
+
+    //7 - Como posso usar uma função de ordem superior para imprimir uma mensagem n vezes em Kotlin?
+    val mensagemx2 = printMensagem(2)
+    val mensagemx10 = printMensagem(10)
+    println(mensagemx2("Hello"))
+    println(mensagemx10("World"))
 }
